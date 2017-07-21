382b2aa6f2b933fcbbf85ed0dcd932942d97ea8d
Ivens Pontes <ivenscardoso@hotmail.com>
2017-07-20 23:25:07 -0300

    fix wrong view name

--- app/Http/Controllers/PlayerController.php
+++ app/Http/Controllers/PlayerController.php
@@ -23,7 +23,7 @@ class PlayerController extends Controller
 
 	public function create()
 	{
-		return view('pages.createcharacter');
+		return view('pages.createCharacter');
 	}
 
 	public function store(CreatePlayer $request)
