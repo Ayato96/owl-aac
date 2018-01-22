517c2f921b4fa745a287a44fbf439b004c01f52a
Ivens Pontes <ivenscardoso@hotmail.com>
2018-01-21 21:47:38 -0300

    add Localization

--- resources/views/pages/account/index.blade.php
+++ resources/views/pages/account/index.blade.php
@@ -1,16 +1,16 @@
 @extends('layouts.owl.app')
 
-@section('title', 'Account Management')
-@section('header', 'Account Management')
+@section('title', @trans('words.account_management'))
+@section('header', @trans('words.account_management'))
 
 @section('content')
 
     <div class="panel panel-primary">
-        <div class="panel-heading">Account Status</div>
+        <div class="panel-heading">@lang('words.account_info')</div>
         <div class="panel-body padding-0">
             <table class="table table-hover table-bordered table-striped margin-bottom-5">
                 <tr>
-                    <td class="col-md-3">Account Name:</td>
+                    <td class="col-md-3">@lang('words.account_name'):</td>
                     <td>{{ $account->name }}</td>
                 </tr>
                 <tr>
@@ -18,7 +18,7 @@
                     <td>{{ $account->email }}</td>
                 </tr>
                 <tr>
-                    <td class="col-md-3">Account Status:</td>
+                    <td class="col-md-3">@lang('words.account_info'):</td>
                     <td>
                         @if ($account->premdays>0)
                             Premium Account
@@ -29,29 +29,29 @@
                 </tr>
             </table>
             <div class="text-center margin-bottom-5">
-                {!! link_to_route('account.change.password', 'Change Password', [], ['class' => 'btn btn-primary btn-sm']) !!}
-                <a href="account/change/email" class="btn btn-primary btn-sm">Change Email</a>
+                {!! link_to_route('account.change.password', @trans('words.change_password'), [], ['class' => 'btn btn-primary btn-sm']) !!}
+                <a href="account/change/email" class="btn btn-primary btn-sm">@lang('words.change_email')</a>
                 @if (!$account->key)
-                    {!! link_to_route('account.show.key', 'Generate Key', [], ['class' => 'btn btn-warning btn-sm']) !!}
+                    {!! link_to_route('account.show.key', @trans('words.generate_key'), [], ['class' => 'btn btn-warning btn-sm']) !!}
                 @endif
             </div>
         </div>
     </div>
 
     <div class="panel panel-primary">
-        <div class="panel-heading">Characters</div>
+        <div class="panel-heading"> @lang('words.characters') </div>
         <div class="panel-body padding-0">
             <table class="table table-hover table-bordered table-striped margin-bottom-5">
                 @if ($players->isEmpty())
                     <div class="text-center">
-                        You don't have characters.
+                        @lang('words.no_characters')
                     </div>
                 @else
                     <tr>
                         <th></th>
-                        <th>Name</th>
-                        <th>Vocation</th>
-                        <th>Level</th>
+                        <th>@lang('words.name')</th>
+                        <th>@lang('words.vocation')</th>
+                        <th>@lang('words.level')</th>
                         <th></th>
                     </tr>
                 @endif
@@ -63,10 +63,10 @@
                         <td>{{ $player->level }}</td>
                         <td class="col-md-3">
                             <div class="text-center">
-                                {!! link_to_route('player.edit', 'Edit',
+                                {!! link_to_route('player.edit', @trans('words.edit'),
                                     [$player->id], ['class' => 'btn btn-primary btn-xs']) !!}
                                 @if (!$player->trashed())
-                                    {!! link_to_route('player.delete', 'Delete',
+                                    {!! link_to_route('player.delete', @trans('words.delete'),
                                         [$player->id], [
                                             'class' => 'btn btn-danger btn-xs',
                                             'onclick'=>"return confirm('Do you really want to delete this character??')",
@@ -81,20 +81,20 @@
                 @endforeach
             </table>
             <div class="pull-right margin-bottom-5 margin-right-5">
-                <a href="{{ route('player.create') }}" class="btn btn-primary btn-sm">Create</a>
+                <a href="{{ route('player.create') }}" class="btn btn-primary btn-sm">@lang('words.create')</a>
             </div>
         </div>
     </div>
     @if(!$account->playersOnlyTrash->isEmpty())
         <div class="panel panel-primary">
-            <div class="panel-heading">Deleted Characters</div>
+            <div class="panel-heading">@lang('words.deleted_characters')</div>
             <div class="panel-body padding-0">
                 <table class="table table-hover table-bordered table-striped margin-bottom-5">
                     <tr>
                         <th></th>
-                        <th>Name</th>
-                        <th>Deletion date</th>
-                        <th>Level</th>
+                        <th>@lang('words.name')</th>
+                        <th>@lang('words.deletion_date')</th>
+                        <th>@lang('words.level')</th>
                         <th></th>
                     </tr>
                     @foreach ($account->playersOnlyTrash as $player)
@@ -105,7 +105,7 @@
                             <td>{{ $player->level }}</td>
                             <td class="col-md-3">
                                 <div class="text-center">
-                                    {!! link_to_route('player.restore', 'Restore',
+                                    {!! link_to_route('player.restore', @trans('words.restore'),
                                         [$player->id], ['class' => 'btn btn-warning btn-xs','onclick'=>"return confirm('Are you sure?')"]) !!}
 
                                 </div>
