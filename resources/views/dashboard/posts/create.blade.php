7be6cade04cb56983f127a24f0672ff0d5948145
Ivens Pontes <ivenscardoso@hotmail.com>
2018-02-10 00:43:30 -0300

    change summernote to tinymce

--- resources/views/dashboard/posts/create.blade.php
+++ resources/views/dashboard/posts/create.blade.php
@@ -24,7 +24,7 @@
 	<div class="form-group {{ $errors->has('content') ? ' has-error' : '' }}">
 		{!! Form::label('content', 'Content', ['class' => 'col-md-4 control-label']) !!}
 		<div class="col-md-6">
-        {{ Form::textarea('content', NULL, ['class' => 'form-control', 'placeholder' => 'Content', 'id' => 'content', 'required']) }}
+        {{ Form::textarea('content', NULL, ['class' => 'form-control my-editor', 'placeholder' => 'Content', 'id' => 'content', 'required']) }}
 			@if ($errors->has('content'))
 			<span class="help-block">
 				<strong>{{ $errors->first('content') }}</strong>
@@ -47,7 +47,7 @@
 
 	<div class="form-group">
 		<div class="col-md-6 col-md-offset-4">
-			{!! Form::submit('Create', ['class'=>'btn btn-primary']) !!}
+			{!! Form::submit('Post', ['class'=>'btn btn-primary']) !!}
 		</div>
 	</div>
 
@@ -57,16 +57,45 @@
 @endsection
 
 @section('css')
-	<link href="//cdnjs.cloudflare.com/ajax/libs/summernote/0.7.0/summernote.css" rel="stylesheet">
+	
 @endsection
 
 @section('js')
-	<script src="//cdnjs.cloudflare.com/ajax/libs/summernote/0.7.0/summernote.js"></script>
+	<script src="//cdn.tinymce.com/4/tinymce.min.js"></script>
 	<script>
-		jQuery(document).ready(function() {
-			jQuery('#content').summernote({
-				height: 250,
+		var editor_config = {
+			path_absolute : "/",
+			selector: "textarea.my-editor",
+			plugins: [
+			"advlist autolink lists link image charmap print preview hr anchor pagebreak",
+			"searchreplace wordcount visualblocks visualchars code fullscreen",
+			"insertdatetime media nonbreaking save table contextmenu directionality",
+			"emoticons template paste textcolor colorpicker textpattern"
+			],
+			toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image media",
+			relative_urls: false,
+			file_browser_callback : function(field_name, url, type, win) {
+			var x = window.innerWidth || document.documentElement.clientWidth || document.getElementsByTagName('body')[0].clientWidth;
+			var y = window.innerHeight|| document.documentElement.clientHeight|| document.getElementsByTagName('body')[0].clientHeight;
+
+			var cmsURL = editor_config.path_absolute + 'laravel-filemanager?field_name=' + field_name;
+			if (type == 'image') {
+				cmsURL = cmsURL + "&type=Images";
+			} else {
+				cmsURL = cmsURL + "&type=Files";
+			}
+
+			tinyMCE.activeEditor.windowManager.open({
+				file : cmsURL,
+				title : 'Filemanager',
+				width : x * 0.8,
+				height : y * 0.8,
+				resizable : "yes",
+				close_previous : "no"
 			});
-		});
-    </script>
+			}
+		};
+
+		tinymce.init(editor_config);
+	</script>
 @endsection
