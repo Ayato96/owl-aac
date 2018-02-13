5059d40d8263b795033f07ed918695a77fc35b71
Ivens Pontes <ivenscardoso@hotmail.com>
2018-02-12 22:44:35 -0300

    temp fix for #5

---
+++ resources/views/layouts/errors.blade.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>@yield('title')</title>
+        <link rel="stylesheet" type="text/css" href="{{ URL::asset('Themes/arch/assets/css/bootstrap.min.css') }}">
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+
+        <style>
+            html, body {
+                height: 100%;
+            }
+
+            body {
+                margin: 0;
+                padding: 0;
+                width: 100%;
+                color: #636b6f;
+                display: table;
+                font-weight: 100;
+                font-family: 'Raleway', sans-serif;
+            }
+
+            .container {
+                text-align: center;
+                display: table-cell;
+                vertical-align: middle;
+            }
+
+            .content {
+                text-align: center;
+                display: inline-block;
+            }
+
+            .title {
+                font-size: 72px;
+                margin-bottom: 40px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <div class="content">
+                <div class="title">@yield('content')</div>
+            </div>
+        </div>
+    </body>
+</html>
