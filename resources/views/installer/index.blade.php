d9134566958440225372c7bbf91d89296b4e176c
Ivens Pontes <ivenscardoso@hotmail.com>
2018-02-20 01:24:37 -0300

    add bootzard
    need work in assets folder

---
+++ resources/views/installer/index.blade.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>OWL WIZARD</title>
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto:400,100,300,500">
+    <link rel="stylesheet" href="{{ asset('assets/bootstrap/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/font-awesome/css/font-awesome.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/form-elements.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+
+</head>
+
+<body>
+<!-- Top content -->
+<div class="top-content">
+    <div class="container">
+
+        <div class="row">
+            <div class="col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2 col-lg-6 col-lg-offset-3 form-box">
+                <form role="form" action="{{route('install.post')}}" method="post" class="f1" id="install">
+                    {{ csrf_field() }}
+
+                    <h3>OwlAAC Wizard</h3>
+                    <div class="f1-steps">
+                        <div class="f1-progress">
+                            <div class="f1-progress-line" data-now-value="16.66" data-number-of-steps="3"
+                                 style="width: 16.66%;"></div>
+                        </div>
+                        <div class="f1-step active">
+                            <div class="f1-step-icon"><i class="fa fa-folder"></i></div>
+                            <p>directory</p>
+                        </div>
+                        <div class="f1-step">
+                            <div class="f1-step-icon"><i class="fa fa-key"></i></div>
+                            <p>account</p>
+                        </div>
+                        <div class="f1-step">
+                            <div class="f1-step-icon"><i class="fa fa-check"></i></div>
+                            <p>finish</p>
+                        </div>
+                    </div>
+
+                    <fieldset>
+                        <h4>Path:</h4>
+                        <div class="form-group">
+                            <input type="text" name="path" placeholder="/home/user/otx/ or C:/otx" class="form-control">
+                        </div>
+                        <div class="f1-buttons">
+                            <button type="button" class="btn btn-next">Next</button>
+                        </div>
+                    </fieldset>
+
+                    <fieldset>
+                        <h4>Set up your admin account:</h4>
+                        <div class="form-group">
+                            <input type="text" name="name" placeholder="Account Name" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <input type="password" name="password" placeholder="Password" class="form-control">
+                        </div>
+                        <div class="f1-buttons">
+                            <button type="button" class="btn btn-previous">Previous</button>
+                            <button type="button" class="btn btn-next">Next</button>
+                        </div>
+                    </fieldset>
+
+                    <fieldset>
+                        <h3>Thank you for download OwlAAC</h3>
+                        <p>
+                            When you click in finish, OwlAAC will be installed.
+                        </p>
+                        <p>
+                            Be patient, it will not be long, just a few seconds.
+                        </p>
+                        <div class="f1-buttons">
+                            <button type="button" class="btn btn-previous">Previous</button>
+                            <button type="submit" class="btn btn-submit">Finish</button>
+                        </div>
+                    </fieldset>
+
+                </form>
+            </div>
+        </div>
+
+    </div>
+</div>
+
+
+<!-- Javascript -->
+<script src="{{ asset('assets/js/jquery-1.11.1.min.js') }}"></script>
+<script src="{{ asset('assets/bootstrap/js/bootstrap.min.js') }}"></script>
+<script type="text/javascript" src="{{ asset('vendor/jsvalidation/js/jsvalidation.js')}}"></script>
+{!! JsValidator::formRequest('App\Http\Requests\Install', '#install') !!}
+<script src="{{ asset('assets/js/jquery.backstretch.min.js') }}"></script>
+<script src="{{ asset('assets/js/retina-1.1.0.min.js') }}"></script>
+<script src="{{ asset('assets/js/scripts.js') }}"></script>
+<script>
+    $(document).ready(function () {
+        $(window).keydown(function (event) {
+            if (event.keyCode === 13) {
+                event.preventDefault();
+                return false;
+            }
+        });
+    });
+</script>
+
+</body>
+
+</html>
