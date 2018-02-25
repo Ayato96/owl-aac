0f382939ffb615ca914bc2e048b119f635af72b7
Ivens Pontes <ivenscardoso@hotmail.com>
2018-02-25 01:16:27 -0300

    Add lang

---
+++ resources/lang/pt-BR/words.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ivens Pontes
+ * Date: 21/01/2018
+ * Time: 14:09
+ */
+
+return [
+
+    'create'                => 'Criar',
+    'restore'               => 'Restaurar',
+    'edit'                  => 'Editar',
+    'delete'                => 'Deletar',
+    'generate_key'          => 'Gerar Chave',
+
+    'name'                  => 'Nome',
+    'vocation'              => 'Vocação',
+    'level'                 => 'Nível',
+    'deletion_date'         => 'Data de exclusão',
+
+    'account'               => 'Conta',
+    'account_management'    => 'Gerenciamento de conta',
+    'account_info'          => 'Informação da conta',
+    'account_name'          => 'Nome da conta',
+    'account_status'        => 'Status da conta',
+    'change_password'       => 'Mudar senha',
+    'change_email'          => 'Mudar Email',
+
+    'characters'            => 'Personagens',
+    'deleted_characters'    => 'Personagens Deletados',
+    'no_characters'         => 'Você não tem personagens',
+    'never_loggedin'        => 'Nunca entrou'
+];
